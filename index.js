ed9c46db77a38cae7e32135fc0a11e6881313540
cruelcaretaker <jashsharma1310@gmail.com>
2024-08-25 11:39:15 +0530

    backend

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data || [];
+  const user_id = "jash_sharma_28032003";
+  const email = "jashsharma1310@gmail.com";
+  const roll_number = "21BCE6176";
+
+  const numbers = data.filter((item) => !isNaN(item));
+  const alphabets = data.filter((item) => isNaN(item));
+  const highest_lowercase_alphabet =
+    alphabets
+      .filter((char) => char >= "a" && char <= "z")
+      .sort()
+      .reverse()[0] || "";
+
+  res.json({
+    is_success: true,
+    user_id: user_id,
+    email: email,
+    roll_number: roll_number,
+    numbers: numbers,
+    alphabets: alphabets,
+    highest_lowercase_alphabet: highest_lowercase_alphabet
+      ? [highest_lowercase_alphabet]
+      : [],
+  });
+});
+
+app.get("/bfhl", (req, res) => {
+  res.json({ operation_code: 1 });
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is running");
+});
